8897771c1f642e72cff97fe125fb7f6008b8d5e1
Elias <elias_amstein@sluz.ch>
2022-12-07 12:56:28 +0100

    add express server in index.ts

---
+++ service/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const SERVER_PORT = 5002;
+
+const app: Express = express();
+
+// CORS Middleware
+app.use(cors());
+
+// Logging Middleware
+app.use(morgan("combined"));
+
+// URL Encoding Middleware
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server listening at http://localhost:${SERVER_PORT}`);
+});
