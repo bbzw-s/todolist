e5352b410d45520351645c24526905ab99ac9a63
Lian Studer <ln.studer@protonmail.ch>
2022-12-09 17:07:03 +0100

    add router to express app

--- service/src/index.ts
+++ service/src/index.ts
@@ -2,6 +2,8 @@ import express, { Express } from "express";
 import cors from "cors";
 import morgan from "morgan";
 
+import todosRouter from "./todos/todos.router";
+
 const SERVER_PORT = 5002;
 
 const app: Express = express();
@@ -14,6 +16,8 @@ app.use(morgan("combined"));
 
 // URL Encoding Middleware
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/todos", todosRouter);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening at http://localhost:${SERVER_PORT}`);
