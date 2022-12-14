f72e2237cfa8eee79aa07cf0240162dce23d4531
Elias <elias_amstein@sluz.ch>
2022-12-14 12:55:43 +0100

    todo update route

--- service/src/todos/todos.controller.ts
+++ service/src/todos/todos.controller.ts
@@ -5,3 +5,17 @@ export const getAllTodos = (_: Request, res: Response) => {
   const todos = Todo.getAll();
   return res.json(todos).status(200);
 };
+
+export const updateTodo = (req: Request, res: Response) => {
+  const todoId = req.params.id;
+  if (!todoId) res.status(400);
+
+  const newTodo: {
+    title: string;
+    done: boolean;
+  } = req.body.todo;
+  if (!newTodo) res.status(400);
+
+  Todo.update(todoId, new Todo(newTodo.title, newTodo.done));
+  return res.status(200);
+};
